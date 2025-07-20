77d0b337d99c6f372042d3b1c72281850234e49d
Perwez087 <parwez4568@gmail.com>
2025-07-20 18:59:13 +0530

    update

--- backend_task/database.py
+++ backend_task/database.py
@@ -8,11 +8,10 @@ DATABASE_NAME = "hro_ecommerce"
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[DATABASE_NAME]
 
-# Collections
+
 product_collection = db["products"]
 order_collection = db["orders"]
 
-# Helper to convert ObjectId to str
 def serialize_mongo_doc(doc):
     doc["_id"] = str(doc["_id"])
     return doc
